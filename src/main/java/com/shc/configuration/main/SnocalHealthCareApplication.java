a433a0fcb160ac79b79562845ab155a6c0dcbeff
Md Sonu Nadaf <sonunadaf88@gmail.com>
2023-03-12 13:08:38 +0530

    messahe

---
+++ src/main/java/com/shc/configuration/main/SnocalHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.shc.configuration.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnocalHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnocalHealthCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shc/configuration/main/SnocalHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shc.configuration.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnocalHealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
